d6801850f6d42a16927e0a92041996fd5f2afed6
Cbih939 <memegold999@gmail.com>
2022-08-10 17:34:18 -0300

    adicionando dependencias gradle e conecção ao DB mysql

---
+++ TodoApp/app/src/main/java/TodoApp/Util/ConnectionFactory.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package TodoApp.Util;
+
+/**
+ *
+ * @author cbih9
+ */
+public class ConnectionFactory {
+    public static final String DRIVER = "com.mysql.jdbc.Driver";
+    public static final String URL = "jdbc:mysql://localhost:3306/todoapp";
+    public static final String USER = "root";
+    public static final String PASS = "";
+}
